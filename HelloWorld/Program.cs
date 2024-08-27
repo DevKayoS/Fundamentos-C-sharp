951764034ef8eda0c023804163bc6c7355d51eb1
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-27 00:38:42 -0300

    Aprendendo a usar classes no C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,20 @@
-﻿namespace HelloWorld;
+﻿using HelloWorld.Teste;
 
+namespace HelloWorld;
+
+class Program
+{
+    // executar sem retornar nada
+    static void Main()
+    {
+        Carro meuCarro = new Carro();
+
+        meuCarro.Ligar();
+        meuCarro.Desligar();
+
+
+        Biscoito meuBiscoito = new Biscoito();
+
+        meuBiscoito.Temperatura();
+    }
+}
