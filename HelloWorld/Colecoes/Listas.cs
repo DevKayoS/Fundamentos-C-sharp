9e5c73392638210de49754c04080c1133f672765
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-28 13:21:11 -0300

    aprendendo mais sobre listas

--- HelloWorld/Colecoes/Listas.cs
+++ HelloWorld/Colecoes/Listas.cs
@@ -16,4 +16,36 @@ public class Listas
         Console.WriteLine(lista[0]);
         Console.WriteLine(lista.Count);
     }
+
+    void UsandoListasMaisDetalhadas()
+    {
+        List<string> strings = new List<string>();
+        strings.Add("Ola");
+        strings.Add("world");
+        strings.Add("Kayo");
+        strings.Add("Vinicius");
+
+        string resultado = $"{strings[0]} {strings[1]}";
+
+        Console.WriteLine(resultado);
+
+        string resultado2 = string.Join(";", strings);
+
+        Console.WriteLine(resultado2);
+        
+        List<int> ints = new List<int>();
+        ints.Add(1);
+        
+        List<decimal> decimals = new List<decimal>();
+        decimals.Add(2.5m);
+        
+        List<bool> bools = new List<bool>();
+        bools.Add(true);
+
+        List<object> objetos = new List<object>();
+        objetos.Add("hello");
+        objetos.Add(12);
+        objetos.Add(true);
+        objetos.Add(2.3);
+    }
 }
