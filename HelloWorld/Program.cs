2bb69acadf4b9e4f7ca8619951b1652884bf01d8
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-29 12:06:42 -0300

    aprendendo a debugar

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,17 @@ class Program
 {
     static void Main()
     {
-   
+        int numero = 10;
+
+        var meuCarro = new Carro("Fuscao Preto")
+        {
+            cor = Cor.Preto,
+            LancadoEm = new DateOnly(1970, 05, 09)                
+        };
         
+        meuCarro.NomeDoModelo();
+
+        Console.WriteLine("teste");
+
     }
 }
