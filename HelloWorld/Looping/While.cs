5b2511d0da2d00a1d6601c1148d132c2bc36bf5a
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-30 12:03:41 -0300

    aprendendo a usar while

---
+++ HelloWorld/Looping/While.cs
@@ -0,0 +1,15 @@
+namespace HelloWorld.Looping;
+
+public class While
+{
+    void UsandoWhile()
+    {
+        var numero = 0;
+        
+        while (numero < 10)
+        {
+            numero++;
+            Console.WriteLine(numero);
+        }
+    }
+}
